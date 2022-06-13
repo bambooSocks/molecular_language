9c43d0b2686e7492f47448055edd0afcafbffb1e
Matej Majtan <majtan.matej@gmail.com>
2022-06-13 13:23:18 +0200

    Added types

--- src/Lib/Parser/Types.fs
+++ src/Lib/Parser/Types.fs
@@ -1,4 +1,40 @@
 namespace Parser
 
 module Types =
-    type Crn = Crn of string
+    type TCrn = Crn of TRootList
+
+    and TRootList =
+        | Root of TRoot
+        | Root_Seq of TRoot * TRootList
+
+    and TRoot =
+        | Conc of TSpecies * TNumber
+        | Step of TCommandList
+
+    and TCommandList =
+        | Command of TCommand
+        | Command_Seq of TCommand * TCommandList
+
+    and TCommand =
+        | Module of TModule
+        | Conditional of TConditional
+    //    | Rxn of TExpr * TExpr * TNumber
+    and TModule =
+        | Ld of TSpecies * TSpecies
+        | Add of TSpecies * TSpecies * TSpecies
+        | Sub of TSpecies * TSpecies * TSpecies
+        | Mul of TSpecies * TSpecies * TSpecies
+        | Div of TSpecies * TSpecies * TSpecies
+        | Sqrt of TSpecies * TSpecies
+        | Cmp of TSpecies * TSpecies
+
+    and TConditional =
+        | IfGT of TCommandList
+        | IfGE of TCommandList
+        | IfEQ of TCommandList
+        | IfLT of TCommandList
+        | IfLE of TCommandList
+
+    and TExpr = Expr of TSpecies * TSpecies list
+    and TSpecies = Species of string
+    and TNumber = Number of int
