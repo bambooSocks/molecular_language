3534924140819ee17d476d024233ac33095b8f65
KristineKlok <55542051+KristineKlok@users.noreply.github.com>
2022-06-14 11:48:00 +0200

    Now supports drawing step graphs as well

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -25,4 +25,10 @@ let small2 = "crn = { conc[a, 32], conc[b, 12], step[{ ifGT[{ sub[atmp,btmp,a] }
 let res = runCrnParser gcd
 printf "%A" res
 
-draw [((fun x -> x*x), "first"); ((fun x -> x*x*x), "second")]
+// Draw functions
+// draw: (function, sting) list -> min -> max ->
+draw [((fun x -> x*x), "first"); ((fun x -> x*x*x), "second")] 0.0 10.0
+
+//Draw steps
+// drawSteps: (float list * string) list -> 
+drawSteps [([1.0; 2.0; 3.0; 4.0; 5.0], "fisrt") ; ([4.0; 3.0; 2.0; 1.0], "second")]
