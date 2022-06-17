17a743586d22e48accdee8f406f16103ea41c287
Matej Majtan <majtan.matej@gmail.com>
2022-06-17 13:51:44 +0200

    Added parser tests

--- src/Tests/Program.fs
+++ src/Tests/Program.fs
@@ -1,2 +1,21 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿namespace Tests
+
+open Parser.Parser
+open FsCheck
+open CustomGenerator
+open Helpers
+
+module Tests =
+
+    let parserProperty ast =
+        let generated = rootListToString ast
+        let parsed = getParserResult (runCrnParser generated)
+
+        if parsed.IsSome then
+            ast = parsed.Value
+        else
+            false
+
+    let _ = Arb.register<customGenerator> ()
+
+    let _ = Check.Verbose parserProperty
