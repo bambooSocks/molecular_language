f455b7ac930d913107088d033de5ffca771311cc
Christopher Acosta <chriz@live.dk>
2022-06-22 16:49:45 +0200

    Simulator code cleanup

--- src/ChemicalReactionSimulator/Program.fs
+++ src/ChemicalReactionSimulator/Program.fs
@@ -8,7 +8,6 @@ open Parser.Parser
 open ChemicalReactions.Samples
 open Drawing 
 
-<<<<<<< HEAD
 let crn1res = simulateN crn1 crn1s0 0.01 1500
 let crn2res = simulateN crn9 crn9s0 0.01 5000
 let ldcrnres = simulateN ldcrn ldcrns0 0.01 1000
@@ -17,7 +16,7 @@ let subcrnres = simulateN subcrn subcrns0 0.01 500
 let divcrnres = simulateN divcrn divcrns0 0.01 1000
 let sqrtcrnres = simulateN sqrtcrn sqrtcrns0 0.01 1000
 let crn7res = simulateN crn7 crn7s0 0.001 1000
-=======
+
 let speciesConcs species states = List.map (Map.find species) states
 let drawStates res =
     drawSteps (List.map (fun s -> (speciesConcs s res, s)) (Seq.toList <| Map.keys res[0]))
@@ -30,44 +29,10 @@ let drawStates res =
 // let divcrnres = simulateN divcrn divcrns0 0.01 1000
 // let sqrtcrnres = simulateN sqrtcrn sqrtcrns0 0.01 1000
 // let crn7res = simulateN crn7 crn7s0 0.001 1000
->>>>>>> 7826df3 (working example for conditionals)
+//let crn9res = simulateN crn9 crn9s0 0.01 10000
 
 
-let oscs x n = List.map (fun i -> (x + string i, 1.0 / float i)) [1..n]
-
-let condcrns0 = Map.ofList <| [("x", 20.0); ("y", 80.0); ("xGTy", 0.5); ("xLTy", 0.5); ("b", 0.0); ("c1", 0.1); ("c2", 0.1); ("h", 0.0)] @ oscs "osc" 6
-open Parser.Types
-let addcrn =
-        let rxn1 = Rxn (["x"], ["x"; "c1"], 1)
-        let rxn2 = Rxn (["y"], ["y"; "c1"], 1)
-        let rxn3 = Rxn (["c1"], [], 1)
-        [rxn1; rxn2; rxn3]
-let subcrn =
-        let rxn1 = Rxn (["y"], ["y"; "c2"], 1)
-        let rxn2 = Rxn (["x"], ["x"; "h"], 1)
-        let rxn3 = Rxn (["c2"], [], 1)
-        let rxn4 = Rxn (["c2"; "h"], [], 1)
-        [rxn1; rxn2; rxn3; rxn4]
-// let ldx =
-//     let rxn1 = Rxn (["x"], ["x"; "xtemp"], 1)
-//     let rxn2 = Rxn (["xtemp"], [], 1)
-//     [rxn1; rxn2]
-// let ldy =
-//     let rxn1 = Rxn (["y"], ["y"; "xtemp"], 1)
-//     let rxn2 = Rxn (["ytemp"], [], 1)
-//     [rxn1; rxn2]
-let condcrn = addOscs crn7 "osc3" @ addOscs crn8 "osc6" @ genOscCrn "osc" 6 @ addOscs addcrn "xGTy" @ addOscs subcrn "xLTy"
-printfn "%A" (Map.toList condcrns0)
-printfn "%A" condcrn
-printfn "%A" oscs
-let condcrnres = simulateN condcrn condcrns0 0.01 1000
-drawStates condcrnres
-printfn "%A" (List.take 3 <| List.map Map.toList condcrnres)
-
-
-//printfn "%A" (genOscCrn "osc" 6)
-// printfn "%A" (addOscs crn7 "osc3")
-// printfn "%A" (addOscs crn8 "osc6")
+//drawStates crn9res
 //drawStates crn1res
 //drawStates crn2res
 //drawStates ldcrnres
@@ -76,6 +41,9 @@ printfn "%A" (List.take 3 <| List.map Map.toList condcrnres)
 //drawStates divcrnres
 //drawStates sqrtcrnres
 //printfn "%A" crn7res
+
+let oscs x n = List.map (fun i -> (x + string i, 1.0 + 0.1 * float i)) [1..n]
+let zeroconcs xs = List.map (fun s -> (s, 0.0)) xs
 
 let parseSimulateDraw src =
     let parseResult = runRxnParser src
@@ -88,3 +56,50 @@ let parseSimulateDraw src =
 
 let rs = "rxn[a + b, a + b + c, 1], rxn[c, , 1]"
 //let parseResult = parseSimulateDraw rs
+
+let gcd =
+        """
+        crn = {
+            conc[a,32],
+            conc[b,12],
+            step[{
+                ld [a, atmp],
+                ld [b, btmp],
+                cmp[a,b]
+            }],
+            step[{
+                ifGT[{ sub[atmp,btmp,a] }],
+                ifLT[{ sub[btmp,atmp,b] }]
+            }]
+        };
+        """
+
+open ChemicalReactions.modulesToReactions
+open Parser.Types
+
+let (Success (ast, _, _)) = runCrnParser gcd
+//printfn "%A" ast
+
+
+let speciesFromRxns rxns =
+    let speciesFromRxn (Rxn (rs, ps, _)) = Set.ofList (rs @ ps)
+    List.fold (fun acc rxn -> Set.union acc (speciesFromRxn rxn)) Set.empty rxns
+let (reactions, _) = toReactionNetwork ast
+let species = Set.toList (speciesFromRxns reactions)
+let initialState =
+    let m = Map.ofList (List.map (fun species -> (species, 0.5)) species)
+    List.fold (fun m (s, c) -> Map.add s c m) m ([("a", 32.0); ("b", 12.0)] @ oscs "osc" 9)
+//printfn "%A" species
+printfn "%A" (Map.toList initialState)
+
+let reactionsWithOscs = reactions @ genOscCrn "osc" 9
+printfn "%A" reactionsWithOscs
+
+let initialState' =
+    Map.ofList [("a", 32.0); ("atmp", 0.0); ("b", 12.0); ("btmp", 0.0); ("cmpB", 0.0);
+    ("cmpGT", 0.5); ("cmpLT", 0.5); ("hatmpbtmpa", 0.0); ("hbtmpatmpb", 0.0);
+    ("osc1", 1.0); ("osc2", 0.01); ("osc3", 0.01); ("osc4", 0.01); ("osc5", 0.01);
+    ("osc6", 0.01); ("osc7", 0.01); ("osc8", 0.01); ("osc9", 0.01)]
+
+let crnresult = simulateN reactionsWithOscs initialState' 0.1 5000
+drawStates crnresult
--- src/Lib/ChemicalReactions/Library.fs
+++ src/Lib/ChemicalReactions/Library.fs
@@ -61,16 +61,6 @@ module Samples =
         let rxn2 = Rxn (["c"; "a"], ["a"; "a"], 1)
         [rxn1a; rxn1b; rxn2]
 
-    let genOscCrn x n =
-        let iToRxn i =
-            let xi = x + string i
-            let xi' = x + string (i % n + 1)
-            Rxn ([xi; xi'], [xi'; xi'], 1)
-        List.map iToRxn [1..n]
-
-    let addOscs crn x = List.map (fun (Rxn (rs, ps, k)) -> Rxn (x :: rs, x :: ps, k)) crn
-
-
     let ldcrns0 = Map.ofList [("a", 7.0); ("b", 2.0)]
     let ldcrn =
         let rxn1 = Rxn (["a"], ["a"; "b"], 1)
@@ -117,36 +107,138 @@ module Samples =
         let rxn4 = Rxn (["b"; "xGTy"], ["xGTy"; "xGTy"], 1)
         [rxn1; rxn2; rxn3; rxn4]
 
-
 module modulesToReactions =
+    // Generate the oscillator CRN with species prefix x and numbering 1..n
+    let genOscCrn x n =
+        let iToRxn i =
+            let xi = x + string i
+            let xi' = x + string (i % n + 1)
+            Rxn ([xi; xi'], [xi'; xi'], 1)
+        List.map iToRxn [1..n]
 
-    let rec toReaction acc (reaction: Parser.Types.TCommand) = 
-        match reaction with
-        | Module(reac) -> 
-            match reac with
-            | Ld(x, y)      -> acc @ [Rxn([x], [x;y], 1.0)] @ [Rxn([y], [], 1.0)]
-            | Add(x, y, z)  -> acc @ [Rxn([x], [x;z], 1.0)] @ [Rxn([y], [y;z], 1.0)] @ [Rxn([z], [], 1.0)]
-            | Sub(x, y, z)  -> acc @ [Rxn([x], [x;z], 1.0)] @ [Rxn([y], [y;"H"], 1.0)] @ [Rxn([z], [], 1.0)] @ [Rxn([z;"H"], [], 1.0)]
-            | Mul(x, y, z)  -> acc @ [Rxn([x;y], [x;y;z], 1.0)] @ [Rxn([z], [], 1.0)]      
-            | Div(x, y, z)  -> acc @ [Rxn([x], [x;z], 1.0)] @ [Rxn([y;z], [y], 1.0)]  
-            | Sqrt(x, y)    -> acc @ [Rxn([x], [x;y], 1.0)] @ [Rxn([y;y], [], 0.5)]  
-            | Cmp(x, y)     -> acc @ [Rxn([x + "gt" + y; y], [x + "lt" + y; y], 1.0)]
-                                   @ [Rxn([x + "lt" + y; x], [x + "gt" + y; x], 1.0)]
-                                   @ [Rxn([x + "gt" + y;x + "lt" + y], [x + "lt" + y; y], 1.0)]
-                                   @ [Rxn([y; x + "lt" + y], [x + "lt" + y; x + "lt" + y], 1.0)]
-                                   @ [Rxn([x + "lt" + y; x + "gt" + y], [x + "gt" + y; y], 1.0)]
-                                   @ [Rxn([y; x + "gt" + y], [x + "gt" + y; x + "gt" + y], 1.0)]
-        | Conditional(reac) -> match reac with
-                                | IfGT(command) -> acc @ List.fold toReaction [] command
-                                | IfGE(command) -> acc @ List.fold toReaction [] command
-                                | IfEQ(command) -> acc @ List.fold toReaction [] command
-                                | IfLT(command) -> acc @ List.fold toReaction [] command
-                                | IfLE(command) -> acc @ List.fold toReaction [] command
+    // Add given species x to both sides of each reaction in the given CRN
+    let addCatalysts crn x = List.map (fun (Rxn (rs, ps, k)) -> Rxn (x :: rs, x :: ps, k)) crn
 
+    // Helper functions for constructing reactions from modules with given species
+    let ld a b =
+        let rxn1 = Rxn ([a], [a; b], 1)
+        let rxn2 = Rxn ([b], [], 1)
+        [rxn1; rxn2]
 
-    let toReactionNetwork' acc elem = 
-            match elem with
-            | Step(modules) -> acc @ [List.fold toReaction [] modules]
-            | _ -> acc
+    let add a b c =
+        let rxn1 = Rxn ([a], [a; c], 1)
+        let rxn2 = Rxn ([b], [b; c], 1)
+        let rxn3 = Rxn ([c], [], 1)
+        [rxn1; rxn2; rxn3]
+
+    let sub a b c h =
+            let rxn1 = Rxn ([a], [a; c], 1)
+            let rxn2 = Rxn ([b], [b; h], 1)
+            let rxn3 = Rxn ([c], [], 1)
+            let rxn4 = Rxn ([c; h], [], 1)
+            [rxn1; rxn2; rxn3; rxn4]
+
+    let mul a b c =
+        let rxn1 = Rxn ([a; b], [a; b; c], 1)
+        let rxn2 = Rxn ([c], [], 1)
+        [rxn1; rxn2]
+
+    let div a b c =
+        let rxn1 = Rxn ([a], [a; c], 1)
+        let rxn2 = Rxn ([b; c], [b], 1)
+        [rxn1; rxn2]
+
+    let sqrt a b =
+        let rxn1 = Rxn ([a], [a; b], 1)
+        let rxn2 = Rxn ([b; b], [], 0.5)
+        [rxn1; rxn2]
+
+    let cmp a b =
+        let aGTb ="cmpGT"
+        let aLTb = "cmpLT"
+        let rxn1 = Rxn ([aGTb; b], [aLTb; b], 1)
+        let rxn2 = Rxn ([aLTb; a], [aGTb;a], 1)
+        [rxn1; rxn2]
+
+    let maj fGT fLT b =
+            let rxn1 = Rxn ([fGT; fLT], [fLT; b], 1)
+            let rxn2 = Rxn ([b; fLT], [fLT; fLT], 1)
+            let rxn3 = Rxn ([fLT; fGT], [fGT; b], 1)
+            let rxn4 = Rxn ([b; fGT], [fGT; fGT], 1)
+            [rxn1; rxn2; rxn3; rxn4]
+
+    // Functions for transforming module types to reactions
+    let cmdToCrn = function
+        | Ld(a, b)      -> ld a b
+        | Add(a, b, c)  -> add a b c
+        | Sub(a, b, c)  -> sub a b c ("h" + a + b + c)
+        | Mul(a, b, c)  -> mul a b c
+        | Div(a, b, c)  -> div a b c  
+        | Sqrt(a, b)    -> sqrt a b  
+        | _ -> failwith "unexpected module"
+    let cmpToCrn = function
+        | Cmp(a, b) -> (cmp a b, maj "cmpGT" "cmpLT" "cmpB")
+        | _ -> failwith "unexpected module"
+    let condToCrn = function
+        | IfGT(cmds) -> addCatalysts (List.collect (fun (Module m) -> cmdToCrn m) cmds) "cmpGT"
+        | IfLT(cmds) -> addCatalysts (List.collect (fun (Module m) -> cmdToCrn m) cmds) "cmpLT"
+        | _ -> failwith "unsupported conditional"
+
+    let isCmp = function | Module (Cmp _) -> true | _ -> false
+
+    // Compile a list of steps into a list of reactions
+    let toReactionNetwork (rootList: Parser.Types.TRoot list) =
+        // Compile a single step
+        let stepToCrns step oscCount =
+            let osc = "osc" + string oscCount // Current oscillator species
+            let (modules, conds) = // Separate modules and conditionals under TCommand type
+                List.fold
+                    (fun (ms, cs) cmd ->
+                        match cmd with
+                        | Module m -> (m :: ms, cs)
+                        | Conditional c -> (ms, c ::cs))
+                    ([], [])
+                    step
+            let (noncmps, cmps) = // Separate cmp module from other modules under TModule type
+                List.fold
+                    (fun (ncs, cs) m ->
+                        match m with
+                        | Cmp _ -> (ncs, m :: cs)
+                        | _ -> (m :: ncs, cs))
+                    ([], [])
+                    modules
+            let bHasCmps = not <| List.isEmpty cmps // Does the cmp module occur in this step?
+            let condCrns = List.collect condToCrn conds // Reactions from a conditional (IfGT/IfLT)
+            if bHasCmps // If the cmp module occurs in the step, we use 6 oscillator species
+            then
+                let crnsNoCmp = List.map cmdToCrn noncmps // Reactions from non-cmp modules
+                let crnsNoCmpWithOscs = List.collect (fun m -> addCatalysts m osc) crnsNoCmp // Reactions from non-cmp modules with oscillators added
+                let (normcrns, amcrns) = // Collection of normalization and AM CRNs (Note: redundant as there can only be one cmp in a step)
+                    List.map cmpToCrn cmps
+                    |> List.fold
+                        (fun (normcrns, amcrns) (normcrn, amcrn) -> (normcrn @ normcrns, amcrn @ amcrns))
+                        ([], [])
+                let oscNext = "osc" + (string (oscCount + 3)) // Next oscillator species for AM network
+                let crns = crnsNoCmpWithOscs @ addCatalysts normcrns osc @ addCatalysts amcrns oscNext @ addCatalysts condCrns oscNext
+                (crns, oscCount + 6) // Increment oscCount by 6 due to cmp
+            else // If no cmp module occurs, use 3 oscillator species
+                // Map modules to reactions and add current oscillator species
+                let crns = List.map cmdToCrn noncmps
+                let crnsWithOscs = List.collect (fun crn -> addCatalysts crn osc) crns
+                (crnsWithOscs @ addCatalysts condCrns osc, oscCount + 3) // Increment oscCount by 3 since no cmp occurred
         
-    let toReactionNetwork (list: Parser.Types.TRoot list) = List.fold toReactionNetwork' [] list
+        // Collect reactions from list of steps
+        let rec stepsToCrns steps oscCount =
+            match steps with
+            | [] -> ([], oscCount)
+            | step::steps' ->
+                let (stepCrns, oscCount') = stepToCrns step oscCount
+                let (stepCrns', oscCount'') = stepsToCrns steps' oscCount'
+                (stepCrns @ stepCrns', oscCount'')
+
+        let cmdLists = // List of steps from under TRoot type
+            List.map (function | Step cmds -> Some cmds | Conc _ -> None) rootList
+            |> List.filter (fun o -> o.IsSome)
+            |> List.map (fun o -> o.Value)
+        
+        stepsToCrns cmdLists 3 // Start with oscillator species number 3
